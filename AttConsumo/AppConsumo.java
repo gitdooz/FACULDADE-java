e5808527c1a0e6624907c7c4ebda2520fc3f2824
Oz Menezes <130259602+ozmenezes@users.noreply.github.com>
2023-11-21 10:16:53 -0300

    Update AppConsumo.java

--- AttConsumo/AppConsumo.java
+++ AttConsumo/AppConsumo.java
@@ -1,10 +1,62 @@
 package AttConsumo;
 
+import java.util.InputMismatchException;
+
+import java.util.Scanner;
+
 public class AppConsumo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		double valorConsumo;
+		do {
+			try {
+				System.out.print("Valor do Consumo: ");
+				valorConsumo = Double.parseDouble(input.nextLine());	
+			}
+			
+			catch(NumberFormatException erro){
+				System.out.println("Valor do consumo deve ser numérico!");
+				continue;
+			}
+			break;
+		}while(true);
+		
+		char tipoCliente;
+		do {
+			System.out.print("Tipo do Cliente[A/F/P]: ");
+			tipoCliente= input.nextLine().charAt(0);	
+			tipoCliente = Character.toUpperCase(tipoCliente);
+			if(tipoCliente != 'A' && tipoCliente != 'F' && tipoCliente != 'P') {
+				System.out.println("Tipo inválido!");
+				continue;
+			}
+			break;
+		}while(true);
+		
+		char tipoConsumo; 
+		
+		do {
+			System.out.print("Tipo do Consumo[C/P]: ");
+			tipoConsumo= input.nextLine().charAt(0);	
+			tipoConsumo = Character.toUpperCase(tipoConsumo);
+			if(tipoConsumo != 'C' && tipoConsumo != 'P') {
+				System.out.println("Tipo inválido!");
+				continue;
+			}
+			break;
+		}while(true);
+		
+		if(tipoConsumo == 'C') {
+			Consumo consumo = new Consumo(valorConsumo);
+			System.out.println(consumo);
+		}
+		if(tipoConsumo == 'P') {
+			Consumo consumo = new Consumo(valorConsumo, tipoCliente);
+			System.out.println(consumo);
+		}
+		
+		input.close();
 	}
 
 }
